528714dfc46553e62479652355516d5cbb95c848
Maksim Askhaev <maksim.askhaev@gmail.com>
2018-01-24 19:04:23 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j;
 
+/**
+ * Class Calculate для вычисления арифметических операций
+ * @author maskhaev
+ * @since 24.01.2018
+ * @version 1
+*/
 public class Calculate{
-
+	/**
+         * Точка входа - метод main
+	 * Вывод строки в консоль
+         * @param args - аргументы
+        */   
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+*Package for calculate task
+*
+* @author Maksim Askhaev (maksim.askhaev@gmail.com)
+* @version $id$
+* @since 0.1
+*/
+package ru.job4j
