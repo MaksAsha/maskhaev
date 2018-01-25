faea0d15da6b826d748f4faaad93fe3237c2a5b9
Maksim Askhaev <maksim.askhaev@gmail.com>
2018-01-25 20:23:42 +0300

    3.2. Максимум из двух чисел [#189][#38824]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,8 +1,19 @@
 package ru.job4j.max;
 
+/**
+ * @author Maksim Askhaev (maksim.askhaev@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Max {
-
-    public int getMax(int first, int second){
+    /**
+     * Возвращает максимальное значение
+     *
+     * @param first  - целое число int
+     * @param second - целое число int
+     * @return максимальное их двух параметров
+     */
+    public int getMax(int first, int second) {
         return first > second ? first : second;
     }
 }
