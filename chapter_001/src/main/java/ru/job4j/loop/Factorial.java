53860c8be3f33869641c2d6944297197a0d823fe
Maksim Askhaev <maksim.askhaev@gmail.com>
2018-01-26 15:50:55 +0300

    4.2. Создать программу вычисляющую факториал. Правка по замечанию

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -5,13 +5,14 @@ public class Factorial {
     public int calc(int n) throws IllegalArgumentException{
         int result = 1;
 
+        if (n < 0) throw new IllegalArgumentException();
+
         if (n > 1) {
             for (int i = 1; i <= n; i++) {
                 result = result * i;
             }
         } else {
-            if (n < 0) throw new IllegalArgumentException();
-            if ((n == 0) || (n == 1)) result = 1;
+            result = 1;
         }
         return result;
     }
