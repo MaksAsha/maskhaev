8e1b4a5f3a782ea78c9b09fef2d31dcb90788c66
Maksim Askhaev <maksim.askhaev@gmail.com>
2018-01-26 13:26:26 +0300

    4.4. Построить пирамиду в псевдографике. [#4412][#38873]

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.loop;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.StringJoiner;
+
+public class PaintTest {
+
+    Paint paint;
+
+    @Before
+    public void setUp() {
+        paint = new Paint();
+    }
+
+    @Test
+    public void testPyramidForHeightFour() throws Exception {
+        String result = paint.pyramid(4);
+        String expected = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("   ^   ")
+                .add("  ^^^  ")
+                .add(" ^^^^^ ")
+                .add("^^^^^^^")
+                .toString();
+        Assert.assertThat(result, Matchers.is(expected));
+    }
+
+    @Test
+    public void testPyramidForHeightTwo() throws Exception {
+        String result = paint.pyramid(2);
+        String expected = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add(" ^ ")
+                .add("^^^")
+                .toString();
+        Assert.assertThat(result, Matchers.is(expected));
+    }
+}
