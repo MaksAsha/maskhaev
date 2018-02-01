2337af1e869b9fa8bf12374871d3e49bf957de3c
Maksim Askhaev <maksim.askhaev@gmail.com>
2018-02-01 11:31:20 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#39628]

--- chapter_002/src/test/java/ru/job4j/tracker/start/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/start/StartUITest.java
@@ -5,6 +5,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.tracker.models.Item;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class StartUITest {
 
     @Test
@@ -44,5 +47,63 @@ public class StartUITest {
         Input input = new StubInput(new String[]{"3", item1.getId(), "7"});
         new StartUI(input, tracker).init();
         Assert.assertThat(tracker.findAll()[0].getName(), Matchers.is("Task2"));
+    }
+
+    @Test
+    public void testWhenFindAll() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("Task1", "Desc1", 1L));
+        Input input = new StubInput(new String[]{"4", "7"});
+        new StartUI(input, tracker).init();
+
+        Assert.assertThat(
+                new String(out.toByteArray()),
+                Matchers.is(new StringBuilder()
+                        .append(System.lineSeparator())
+                        .append("Меню:")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 1 - добавить задачу")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 2 - изменить задачу")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 3 - удалить задачу")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 4 - просмотр всех задач")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 5 - поиск задачи по имени")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 6 - поиск задачи по id")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 7 - выйти из программы")
+                        .append(System.lineSeparator())
+                        .append("Список задач:")
+                        .append(System.lineSeparator())
+                        .append(new StringBuilder(item1.toString()))
+                        .append(System.lineSeparator())
+                        .append(System.lineSeparator())
+                        .append("Меню:")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 1 - добавить задачу")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 2 - изменить задачу")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 3 - удалить задачу")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 4 - просмотр всех задач")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 5 - поиск задачи по имени")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 6 - поиск задачи по id")
+                        .append(System.lineSeparator())
+                        .append("Нажмите 7 - выйти из программы")
+                        .append(System.lineSeparator())
+                        .toString())
+        );
+        System.setOut(stdout);
     }
 }
